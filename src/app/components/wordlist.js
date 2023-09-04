555e6340b26e4bcef6334d0d57423a914a9bffda
Chuan Igor <38382865+Syggnus@users.noreply.github.com>
2023-09-04 19:14:36 -0300

    feat: Criação da Wordlist

---
+++ src/app/components/wordlist.js
@@ -0,0 +1,122 @@
+export const wordlist = [
+  {
+    palavra: "Algoritmo",
+    dica: "Conjunto de passos bem definidos para resolver um problema.",
+  },
+  {
+    palavra: "Protocolo",
+    dica: "Regras e convenções que governam a comunicação entre dispositivos em redes de computadores.",
+  },
+  {
+    palavra: "Loop",
+    dica: "Estrutura de controle que repete um bloco de código até que uma condição seja atendida.",
+  },
+  {
+    palavra: "Firewall",
+    dica: "Dispositivo de segurança que controla o tráfego de rede, permitindo ou bloqueando com base em regras predefinidas.",
+  },
+  {
+    palavra: "Variável",
+    dica: "Um local para armazenar dados em um programa de computador.",
+  },
+  {
+    palavra: "Roteador",
+    dica: "Dispositivo de rede que encaminha pacotes de dados entre redes.",
+  },
+  {
+    palavra: "Recursividade",
+    dica: "Técnica em programação onde uma função chama a si mesma para resolver um problema.",
+  },
+  {
+    palavra: "IP Address",
+    dica: "Endereço numérico que identifica um dispositivo em uma rede IP.",
+  },
+  {
+    palavra: "Decisão",
+    dica: "Processo de escolher entre alternativas em um programa de computador.",
+  },
+  {
+    palavra: "Latência",
+    dica: "Atraso percebido na transmissão de dados em uma rede.",
+  },
+  {
+    palavra: "Programação",
+    dica: "Processo de escrever código para criar software ou resolver problemas computacionais.",
+  },
+  {
+    palavra: "Gateway",
+    dica: "Dispositivo que conecta redes diferentes e encaminha pacotes de dados entre elas.",
+  },
+  {
+    palavra: "Laço Infinito",
+    dica: "Um erro em programação onde um loop nunca termina devido a uma condição incorreta.",
+  },
+  {
+    palavra: "Criptografia",
+    dica: "Técnica de proteger informações tornando-as ilegíveis sem a chave de descriptografia correspondente.",
+  },
+  {
+    palavra: "Depuração",
+    dica: "Processo de identificar e corrigir erros em um programa de computador.",
+  },
+  {
+    palavra: "Servidor",
+    dica: "Computador ou software que fornece serviços ou recursos para outros dispositivos em uma rede.",
+  },
+  {
+    palavra: "Desvio Condicional",
+    dica: "Instrução de controle que altera o fluxo do programa com base em uma condição específica.",
+  },
+  {
+    palavra: "Sistema de Nomes de Domínio",
+    dica: "Protocolo que traduz nomes de domínio em endereços IP.",
+  },
+  {
+    palavra: "Linguagem de Máquina",
+    dica: "Linguagem de programação de nível mais baixo compreendida diretamente por um computador.",
+  },
+  {
+    palavra: "Switch",
+    dica: "Dispositivo de rede que encaminha pacotes de dados em uma rede local com base em endereços MAC.",
+  },
+  {
+    palavra: "Loop Infinito",
+    dica: "Um erro em programação onde um loop nunca termina devido a uma condição incorreta.",
+  },
+  {
+    palavra: "Criptografia Assimétrica",
+    dica: "Método de criptografia que envolve um par de chaves: uma pública para criptografar e uma privada para descriptografar.",
+  },
+  {
+    palavra: "IDE",
+    dica: "Software que fornece ferramentas integradas para desenvolvimento de software, incluindo edição, depuração e compilação.",
+  },
+  {
+    palavra: "Proxy",
+    dica: "Servidor intermediário que atua como intermediário entre um cliente e um servidor de destino, controlando e filtrando o tráfego.",
+  },
+  {
+    palavra: "Pilha",
+    dica: "Estrutura de dados que segue a abordagem Last-In-First-Out (LIFO) para armazenamento e recuperação de dados.",
+  },
+  {
+    palavra: "Latência de Rede",
+    dica: "O tempo que leva para um pacote de dados viajar de um ponto a outro em uma rede, muitas vezes medido em milissegundos (ms).",
+  },
+  {
+    palavra: "Loop For",
+    dica: "Tipo de loop em programação que itera sobre uma sequência de valores por um número fixo de vezes.",
+  },
+  {
+    palavra: "Firewall de Aplicação",
+    dica: "Firewall especializado que protege aplicativos web detectando e bloqueando ataques específicos.",
+  },
+  {
+    palavra: "API",
+    dica: "Conjunto de regras e protocolos que permite que diferentes softwares se comuniquem entre si.",
+  },
+  {
+    palavra: "Roteamento Estático",
+    dica: "Método de configuração de rotas de rede manualmente em um roteador ou dispositivo de rede.",
+  },
+];
